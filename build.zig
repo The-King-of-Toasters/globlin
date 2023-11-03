61924270127713e3b4fa5b6ace27abdaf73ef34d
Stephen Gregoratto <dev@sgregoratto.me>
2023-11-04 00:16:24 +1100

    Remake build.zig after Zig 0.11 changes

--- build.zig
+++ build.zig
@@ -1,14 +1,22 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) !void {
-    const mode = b.standardReleaseOptions();
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary("globlin", "src/glob.zig");
-    lib.setBuildMode(mode);
-    lib.install();
+    const lib = b.addStaticLibrary(.{
+        .name = "globlin",
+        .root_source_file = .{ .path = "src/glob.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(lib);
 
-    const main_tests = b.addTest("src/glob.zig");
-    main_tests.setBuildMode(mode);
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/glob.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
     const coverage = b.option(bool, "kcov", "Generate test coverage with kcov") orelse false;
     if (coverage) {
@@ -20,18 +28,25 @@ pub fn build(b: *std.build.Builder) !void {
         });
     }
 
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    const run_main_tests = b.addRunArtifact(main_tests);
 
-    const fuzz_lib = b.addStaticLibrary("fuzz-lib", "src/fuzz.zig");
-    fuzz_lib.setBuildMode(.Debug);
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&run_main_tests.step);
+
+    const fuzz_lib = b.addStaticLibrary(.{
+        .name = "globlin-fuzz",
+        .root_source_file = .{ .path = "src/glob.zig" },
+        .target = target,
+        .optimize = .Debug,
+    });
     fuzz_lib.want_lto = true;
     fuzz_lib.bundle_compiler_rt = true;
+    fuzz_lib.force_pic = true;
 
     const fuzz_executable_name = "fuzz";
     const fuzz_exe_path = try std.fs.path.join(
         b.allocator,
-        &.{ b.cache_root, fuzz_executable_name },
+        &.{ b.cache_root.path.?, fuzz_executable_name },
     );
 
     const fuzz_compile = b.addSystemCommand(&.{ "afl-clang-lto", "-o", fuzz_exe_path });
@@ -45,9 +60,13 @@ pub fn build(b: *std.build.Builder) !void {
     fuzz_compile_run.dependOn(&fuzz_install.step);
 
     // Compile a companion exe for debugging crashes
-    const fuzz_debug_exe = b.addExecutable("fuzz-debug", "src/fuzz.zig");
-    fuzz_debug_exe.setBuildMode(.Debug);
+    const fuzz_debug_exe = b.addExecutable(.{
+        .name = "fuzz-debug",
+        .root_source_file = .{ .path = "src/fuzz.zig" },
+        .target = target,
+        .optimize = .Debug,
+    });
     // Only install fuzz-debug when the fuzz step is run
-    const install_fuzz_debug_exe = b.addInstallArtifact(fuzz_debug_exe);
+    const install_fuzz_debug_exe = b.addInstallArtifact(fuzz_debug_exe, .{});
     fuzz_compile_run.dependOn(&install_fuzz_debug_exe.step);
 }
